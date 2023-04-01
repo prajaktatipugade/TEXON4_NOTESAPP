a3334fc9aa740fbdeee15895c2ea98b31b6088a5
prajaktatipugade <prajaktatipugade82@gmail.com>
2023-04-01 14:20:12 +0530

    finished

---
+++ app/src/main/java/com/example/notes/splash.java
@@ -0,0 +1,33 @@
+package com.example.notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if(currentUser==null){
+                    startActivity(new Intent(splash.this,Login.class));
+                }else{
+                    startActivity(new Intent(splash.this,MainActivity.class));
+                }
+                finish();
+            }
+        },1000);
+
+    }
+}
